07b52706c6f0d8541e5dcb015ab6e135d3e12122
gracia018-cmd <gracia.018@ski.sch.id>
2026-04-14 14:42:57 +0700

    first commit

---
+++ app/core/Router.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Core;
+
+class Router
+{
+    private array $routes = [];
+
+    public function add(string $method, string $uri, string $controller, string $function)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller,
+            'function' => $function,
+        ];
+    }
+
+    public function run()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+        foreach ($this->routes as $route) {
+            $pattern = str_replace(
+                '{id}',
+                '([0-9]+)',
+                $route['uri'],
+            );
+            $pattern = '#^' . $pattern . '$#';
+            // /students/([0-9]+)
+
+            if (preg_match($pattern, $uri, $matches)) {
+                array_shift($matches);
+                require_once '../app/controllers/' . $route['controller'] . '.php';
+
+                $controllerClass = 'App\\Controllers\\' . $route['controller'];
+                $controller = new $controllerClass();
+                $function = $route['function'];
+
+                call_user_func_array([$controller, $function], $matches);
+                // shortcut: index($parameter1, $parameter2, dst);
+                // example: call_user_func_array(['StudentController', 'index'], [1,2])
+                return;
+            }
+        }
+
+        http_response_code(404);
+        echo '<h1>404 - Page Not Found</h1>';
+
+    }
+
+}
+
+?>
